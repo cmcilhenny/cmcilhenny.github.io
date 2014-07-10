7cd87fc54cba2b5ec9c569ae5cb4fb2e1c8b4804
courtney mcilhenny <github@courtneymcilhenny>
2014-07-09 21:12:35 -0700

    adding carat back in with jquery

--- javaScripts/application.js
+++ javaScripts/application.js
@@ -1,17 +1,2 @@
-window.onload = () {
-
-	var navs = document.querySelector('.blog-nav-item');
-	var anchors = navs.getElementsByTagName('a');
-
-	for (var i = 0; i < anchors.length; i += 1) {
-	  anchors[i].addEventListener('click', function() { clickHandler(anchors[i]) }, false);
-	}
-
-	function clickHandler(anchor) {
-	  var hasClass = anchor.getAttribute('class');
-	  if (hasClass !== 'active') {
-	    anchor.setAttribute('class', 'active');
-	  }
-	}
-}
+// placeholder
 
